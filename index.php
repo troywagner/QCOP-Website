38a6fd6afe7cc59bc12cafb3435c24ba42518164
Keith Weaver <keithmweaver@gmail.com>
2014-11-13 09:20:51 -0500

    Updated the landing image so its smaller.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<style>
 		body{
         
-	        background: url(./imgs/LANDING.png) no-repeat center center fixed; 
+	        background: url(./imgs/LANDING.jpg) no-repeat center center fixed; 
 	        -webkit-background-size: cover;
 	        -moz-background-size: cover;
 	        -o-background-size: cover;
